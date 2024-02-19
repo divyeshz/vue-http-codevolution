ba4c6c88f331e87f4b3f868a8363dc7a76b51b97
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-19 13:07:08 +0530

    HTTP GET Request using axios library

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PostList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostList from './components/PostList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostList
   }
 }
 </script>
